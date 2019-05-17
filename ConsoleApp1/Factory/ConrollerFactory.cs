7b870e1ca7f18426205c6dd57b9bffa5866dce00
mtk57 <mtk.kawa@gmail.com>
2019-05-17 21:53:22 +0900

    a

---
+++ ConsoleApp1/Factory/ConrollerFactory.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1.Controller;
+using ConsoleApp1.Interface.Contoller;
+
+namespace ConsoleApp1.Factory
+{
+    public static class ConrollerFactory
+    {
+        public static IContoller GetContoller()
+        {
+            return new ServiceContoller();
+        }
+    }
+}
