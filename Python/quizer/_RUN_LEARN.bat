ca07edca6eae52269d2034a24b9d44b17ada0761
mtk57 <mtk.kawa@gmail.com>
2021-04-18 20:42:51 +0900

    a

--- Python/quizer/_RUN_LEARN.bat
+++ Python/quizer/_RUN_LEARN.bat
@@ -1,2 +1,2 @@
-python.exe main.py --info_path C:\_git\Memo\Other\Salesforce\QUIZ.xlsx --mode=learn
+python.exe main.py --info_path C:\_git\Memo\Other\Salesforce\QUIZ.xlsx --mode=learn --random
 pause
