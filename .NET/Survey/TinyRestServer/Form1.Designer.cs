6989632ae5a03f2839a88a41b8dd8892db6db834
mtk57 <mtk.kawa@gmail.com>
2022-04-21 21:02:26 +0900

    0.1

---
+++ .NET/Survey/TinyRestServer/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace TinyRestServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Url = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox_Timeout = new System.Windows.Forms.TextBox();
+            this.textBox_Method = new System.Windows.Forms.TextBox();
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(22, 601);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(204, 54);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(561, 601);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(204, 54);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(22, 59);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(145, 31);
+            this.textBox_Port.TabIndex = 2;
+            this.textBox_Port.Text = "7016";
+            this.toolTip1.SetToolTip(this.textBox_Port, "Ex:70890");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port";
+            // 
+            // textBox_Url
+            // 
+            this.textBox_Url.Location = new System.Drawing.Point(187, 59);
+            this.textBox_Url.Name = "textBox_Url";
+            this.textBox_Url.Size = new System.Drawing.Size(578, 31);
+            this.textBox_Url.TabIndex = 4;
+            this.textBox_Url.Text = "http://*";
+            this.toolTip1.SetToolTip(this.textBox_Url, "http or https");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "URL";
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.Location = new System.Drawing.Point(22, 138);
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.Size = new System.Drawing.Size(145, 31);
+            this.textBox_Status.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.textBox_Status, "Ex:200");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Http Status Code";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 387);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(743, 186);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Response Data (JSON)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(311, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(399, 31);
+            this.textBox4.TabIndex = 16;
+            this.toolTip1.SetToolTip(this.textBox4, "Ex:123,456");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(249, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Ary";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 31);
+            this.textBox3.TabIndex = 14;
+            this.toolTip1.SetToolTip(this.textBox3, "0 or 1");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Bool";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(311, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(399, 31);
+            this.textBox2.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.textBox2, "Ex:apple");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Str";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Num";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 31);
+            this.textBox1.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.textBox1, "Ex:123456");
+            // 
+            // textBox_Timeout
+            // 
+            this.textBox_Timeout.Location = new System.Drawing.Point(275, 138);
+            this.textBox_Timeout.Name = "textBox_Timeout";
+            this.textBox_Timeout.Size = new System.Drawing.Size(145, 31);
+            this.textBox_Timeout.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.textBox_Timeout, "Ex:30");
+            // 
+            // textBox_Method
+            // 
+            this.textBox_Method.Location = new System.Drawing.Point(491, 138);
+            this.textBox_Method.Name = "textBox_Method";
+            this.textBox_Method.ReadOnly = true;
+            this.textBox_Method.Size = new System.Drawing.Size(145, 31);
+            this.textBox_Method.TabIndex = 11;
+            this.textBox_Method.Text = "GET";
+            this.toolTip1.SetToolTip(this.textBox_Method, "GET");
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Location = new System.Drawing.Point(22, 676);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.ReadOnly = true;
+            this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Log.Size = new System.Drawing.Size(743, 183);
+            this.textBox_Log.TabIndex = 17;
+            this.toolTip1.SetToolTip(this.textBox_Log, "Ex:123,456");
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(271, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 24);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Timeout (sec)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.groupBox2.Location = new System.Drawing.Point(22, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(743, 186);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Request Data";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(487, 111);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(84, 24);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Method";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 875);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.textBox_Method);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_Timeout);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Url);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(826, 946);
+            this.MinimumSize = new System.Drawing.Size(826, 946);
+            this.Name = "Form1";
+            this.Text = "Tiny REST Server";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Url;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Status;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox_Timeout;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_Method;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox_Log;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ .NET/Survey/TinyRestServer/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Forms;
+
+/// <summary>
+/// 簡易RESTサーバ
+/// 
+/// [memo]
+/// ・exeを実行すると「アクセスが拒否されました」と出る場合は、マニフェストファイルのrequestedExecutionLevelのレベルを「requireAdministrator」に変更する。
+///   参考：https://sweep3092.hatenablog.com/entry/2014/12/27/160005
+/// 
+/// [TODO]
+/// MUST
+/// ・Start押しても固まらないようにしよう!
+/// ・Stopに対応しよう!
+/// ・httpsに対応しよう!
+/// ・Vistaで動くか試そう!
+/// ・ステータスコードに対応しよう!
+/// ・タイムアウトに対応しよう!
+/// 
+/// WANT
+/// ・GET以外にも対応しよう!
+/// ・動的にJSONを変更しよう!
+/// 
+/// </summary>
+namespace TinyRestServer
+{
+    public partial class Form1 : Form
+    {
+        private HttpListener listener = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                backgroundWorker1.CancelAsync();
+            }
+
+            if (listener != null)
+            {
+                listener.Stop();
+                listener.Close();
+                listener = null;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            listener = new HttpListener();
+ 
+            // TODO:リッスンするホスト名とポートを指定します。
+            //listener.Prefixes.Add("http://*:7016/");
+
+            listener.Prefixes.Add(textBox_Url.Text + ":" + textBox_Port.Text + @"/");
+
+            listener.Start();
+
+            for (; ; )
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                var c = listener.GetContext();
+
+                var req = c.Request;
+
+                // TODO:ここでURLを切り分けます
+                Console.WriteLine(req.RawUrl);
+
+                if (req.HasEntityBody)
+                {
+                    // TODO：ここでリクエストボディに対する処理を行います。
+                    using (var sr = new StreamReader(req.InputStream, new UTF8Encoding(false)))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                }
+
+                // JSONとして返したい値
+                var value = new Result() { Value = true, Message = "あいうえおabcde" };
+
+                // レスポンスにJSONを書き込みます。
+                HttpListenerResponse res = null;
+                try
+                {
+                    res = c.Response;
+                    res.ContentType = "application/json";
+                    new DataContractJsonSerializer(typeof(Result)).WriteObject(res.OutputStream, value);
+                }
+                finally
+                {
+                    res.Close();
+
+                }
+            }
+            
+        }
+    }
+
+    public class Result
+    {
+        public bool Value { get; set; }
+
+        public string Message { get; set; }
+    }
+}
