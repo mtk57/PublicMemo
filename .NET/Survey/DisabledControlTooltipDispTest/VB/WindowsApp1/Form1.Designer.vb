fcc98939bce7379201eaba345f43e3dbe23316e4
mtk57 <mtk.kawa@gmail.com>
2023-09-02 08:05:47 +0900

    a

---
+++ .NET/Survey/DisabledControlTooltipDispTest/VB/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.checkBox1 = New System.Windows.Forms.CheckBox()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(80, 64)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(270, 91)
+        Me.button1.TabIndex = 1
+        Me.button1.Text = "button1"
+        Me.ToolTip1.SetToolTip(Me.button1, "Hello button1")
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'checkBox1
+        '
+        Me.checkBox1.AutoSize = True
+        Me.checkBox1.Location = New System.Drawing.Point(395, 96)
+        Me.checkBox1.Name = "checkBox1"
+        Me.checkBox1.Size = New System.Drawing.Size(201, 28)
+        Me.checkBox1.TabIndex = 2
+        Me.checkBox1.Text = "Enabled Change"
+        Me.checkBox1.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Enabled = False
+        Me.button2.Location = New System.Drawing.Point(90, 225)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(260, 92)
+        Me.button2.TabIndex = 15
+        Me.button2.Text = "button2"
+        Me.ToolTip1.SetToolTip(Me.button2, "Hello button2")
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 710)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.checkBox1)
+        Me.Controls.Add(Me.button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents button1 As Button
+    Friend WithEvents checkBox1 As CheckBox
+    Private WithEvents button2 As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ .NET/Survey/DisabledControlTooltipDispTest/VB/WindowsApp1/Form1.vb
@@ -0,0 +1,116 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim td As New ToolTipOnDisabledControl()
+        td.SetToolTip(Me.button2, Me.toolTip1, "2")
+    End Sub
+
+	Private Sub checkBox1_CheckedChanged(sender As Object, e As EventArgs) Handles checkBox1.CheckedChanged
+		button1.Enabled = Not button1.Enabled
+
+		If Not button1.Enabled Then
+			Dim td As New ToolTipOnDisabledControl()
+			'td.SetToolTip(this.button1, "1");
+			'test
+			td.SetToolTip(Me.button1, Me.ToolTip1, "1")
+		Else
+			'test
+			Me.ToolTip1.SetToolTip(Me.button1, "Hello button1")
+		End If
+	End Sub
+
+	''' <summary>
+	''' // Reference example
+	'''  var td = new ToolTipOnDisabledControl();
+	'''  this.checkEdit3.Enabled = false;
+	'''  td.SetTooltip(this.checkEdit3, "tooltip for disabled");
+	''' </summary>
+	Public Class ToolTipOnDisabledControl
+#Region "Fields and Properties"
+
+		Private enabledParentControl As Control
+
+		Private isShown As Boolean
+
+		Public Property TargetControl() As Control
+
+		Public Property TooltipText() As String
+		Public ReadOnly Property ToolTip() As ToolTip
+#End Region
+
+#Region "Public Methods"
+		Public Sub New()
+			Me.ToolTip = New ToolTip()
+		End Sub
+
+		'public void SetToolTip(Control targetControl, string tooltipText = null)
+		Public Sub SetToolTip(targetControl As Control, tip As ToolTip, Optional tooltipText As String = Nothing)
+			'test
+			Me.TargetControl = targetControl
+			If String.IsNullOrEmpty(tooltipText) Then
+				Me.TooltipText = Me.ToolTip.GetToolTip(targetControl)
+			Else
+				Me.TooltipText = tooltipText
+
+				' test
+				tip.SetToolTip(targetControl, "")
+			End If
+
+			If targetControl.Enabled Then
+				Me.enabledParentControl = Nothing
+				Me.isShown = False
+				Me.ToolTip.SetToolTip(Me.TargetControl, Me.TooltipText)
+				Return
+			End If
+
+			Me.enabledParentControl = targetControl.Parent
+			While Not Me.enabledParentControl.Enabled AndAlso Me.enabledParentControl.Parent IsNot Nothing
+				Me.enabledParentControl = Me.enabledParentControl.Parent
+			End While
+
+			If Not Me.enabledParentControl.Enabled Then
+				Throw New Exception("Failed to set tool tip because failed to find an enabled parent control.")
+			End If
+
+			AddHandler Me.enabledParentControl.MouseMove, AddressOf Me.EnabledParentControl_MouseMove
+			AddHandler Me.TargetControl.EnabledChanged, AddressOf Me.TargetControl_EnabledChanged
+		End Sub
+
+		Public Sub Reset()
+			If Me.TargetControl IsNot Nothing Then
+				Me.ToolTip.Hide(Me.TargetControl)
+				RemoveHandler Me.TargetControl.EnabledChanged, AddressOf Me.TargetControl_EnabledChanged
+				Me.TargetControl = Nothing
+			End If
+
+			If Me.enabledParentControl IsNot Nothing Then
+				RemoveHandler Me.enabledParentControl.MouseMove, AddressOf Me.EnabledParentControl_MouseMove
+				Me.enabledParentControl = Nothing
+			End If
+
+			Me.isShown = False
+		End Sub
+#End Region
+
+#Region "Private Methods"
+		Private Sub EnabledParentControl_MouseMove(sender As Object, e As MouseEventArgs)
+			If e.Location.X >= Me.TargetControl.Left AndAlso e.Location.X <= Me.TargetControl.Right AndAlso e.Location.Y >= Me.TargetControl.Top AndAlso e.Location.Y <= Me.TargetControl.Bottom Then
+				If Not Me.isShown Then
+					Me.ToolTip.Show(Me.TooltipText, Me.TargetControl, Me.TargetControl.Width / 2, Me.TargetControl.Height / 2, Me.ToolTip.AutoPopDelay)
+					Me.isShown = True
+				End If
+			Else
+				'else if(this.isShown)
+				Me.ToolTip.Hide(Me.TargetControl)
+				Me.isShown = False
+			End If
+		End Sub
+
+		Private Sub TargetControl_EnabledChanged(sender As Object, e As EventArgs)
+			If TargetControl.Enabled Then
+				RemoveHandler TargetControl.EnabledChanged, AddressOf TargetControl_EnabledChanged
+				RemoveHandler enabledParentControl.MouseMove, AddressOf EnabledParentControl_MouseMove
+			End If
+		End Sub
+#End Region
+	End Class
+End Class
