ad181eba1fe9451cbd84b65599edf6b99ca18cea
mtk57 <mtk.kawa@gmail.com>
2023-10-21 09:44:09 +0900

    a

---
+++ .NET/Survey/TextBoxCopyHookVB/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBoxEx1 = New WindowsApp1.TextBoxEx()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxEx2 = New WindowsApp1.TextBoxEx()
+        Me.SuspendLayout()
+        '
+        'TextBoxEx1
+        '
+        Me.TextBoxEx1.Location = New System.Drawing.Point(99, 85)
+        Me.TextBoxEx1.Name = "TextBoxEx1"
+        Me.TextBoxEx1.Size = New System.Drawing.Size(249, 31)
+        Me.TextBoxEx1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 199)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(247, 31)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBoxEx2
+        '
+        Me.TextBoxEx2.Location = New System.Drawing.Point(404, 85)
+        Me.TextBoxEx2.Name = "TextBoxEx2"
+        Me.TextBoxEx2.Size = New System.Drawing.Size(249, 31)
+        Me.TextBoxEx2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1069, 450)
+        Me.Controls.Add(Me.TextBoxEx2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBoxEx1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxEx1 As TextBoxEx
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBoxEx2 As TextBoxEx
+End Class
---
+++ .NET/Survey/TextBoxCopyHookVB/TextBoxEx.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Runtime.InteropServices
+
+''' <summary>
+''' コピー/カット操作をフックして、クリップボードの内容を改変するカスタムテキストボックス
+''' </summary>
+Public Class TextBoxEx
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SetWindowLong(hWnd As IntPtr, nIndex As Integer, newWndProc As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function CallWindowProc(wndProc As IntPtr, hWnd As IntPtr, msg As UInteger, wParam As IntPtr, lParam As IntPtr) As IntPtr
+    End Function
+
+    Private Delegate Function WndProcCallbackDelegate(hWnd As IntPtr, msg As UInteger, wParam As IntPtr, lParam As IntPtr) As IntPtr
+
+    Private Const GWL_WNDPROC As Integer = -4   'ウィンドウプロシージャの新しいアドレスを設定します
+    Private Const WM_CUT As Integer = &H300     'ウィンドウメッセージID(カット)
+    Private Const WM_COPY As Integer = &H301    'ウィンドウメッセージID(コピー)
+
+    Private _myWndHandle As IntPtr
+    Private _isStartedWndProc As Boolean = False
+
+    Public Sub New()
+        InitializeComponent()
+
+        'If DesignMode Then
+        '    Return     '効果がなかった...orz
+        'End If
+    End Sub
+
+    ''' <summary>
+    ''' Enterイベントハンドラ
+    ''' </summary>
+    ''' <param name="e">イベント引数</param>
+    Protected Overrides Sub OnEnter(e As EventArgs)
+        If Not _isStartedWndProc Then
+            'コンストラクタでStartWndProc()を呼ぶとVisualStudioのデザイナが落ちるのでここで呼ぶ
+            _isStartedWndProc = True
+            StartWndProc()
+        End If
+
+        MyBase.OnEnter(e)
+    End Sub
+
+    Private Sub StartWndProc()
+        'コントロールのウィンドウプロシージャをフックする
+        _myWndHandle = SetWindowLong(
+                            MyBase.Handle,
+                            GWL_WNDPROC,
+                            Marshal.GetFunctionPointerForDelegate(New WndProcCallbackDelegate(AddressOf WndProcCallback)))
+    End Sub
+
+    ''' <summary>
+    ''' ウィンドウに送信されたメッセージを処理する、アプリケーションで定義するコールバック関数
+    ''' </summary>
+    ''' <param name="hWnd">ウィンドウへのハンドル</param>
+    ''' <param name="msg">メッセージ</param>
+    ''' <param name="wParam">追加のメッセージ情報</param>
+    ''' <param name="lParam">追加のメッセージ情報</param>
+    ''' <returns>メッセージ処理の結果</returns>
+    Private Function WndProcCallback(hWnd As IntPtr, msg As UInteger, wParam As IntPtr, lParam As IntPtr) As IntPtr
+        If msg = WM_COPY Then
+            Clipboard.SetText(MyBase.Text & "_COPY")    '改変
+            Return IntPtr.Zero ' デフォルトのコピー操作をキャンセル
+        End If
+
+        If msg = WM_CUT Then
+            Clipboard.SetText(MyBase.SelectedText & "_CUT")    '改変
+            MyBase.SelectedText = String.Empty
+            Return IntPtr.Zero ' デフォルトのカット操作をキャンセル
+        End If
+
+        ' 上記以外のメッセージはデフォルトのウィンドウプロシージャに移譲
+        Return CallWindowProc(_myWndHandle, hWnd, msg, wParam, lParam)
+    End Function
+End Class
