94f19dc74f0462601cc6df74dbe5ea5699447d7d
mtk57 <mtk.kawa@gmail.com>
2024-06-23 08:06:13 +0900

    a

--- .NET/Survey/SimpleLogger/Logger.vb
+++ .NET/Survey/SimpleLogger/Logger.vb
@@ -4,7 +4,7 @@ Module SimpleLogger
         Dim file_num As Integer
         file_num = FreeFile()
         FileOpen(file_num, "SimpleLogger.log", OpenMode.Append)
-        PrintLine(file_num, VB6.Format(Today, "yyyy/mm/dd") & " " & VB6.Format(Now, "hh:mm:ss") & ":" & contents)
+        PrintLine(file_num, Format(Today, "yyyy/mm/dd") & " " & Format(Now, "hh:mm:ss") & ":" & contents)
         FileClose(file_num)
         file_num = -1
     End Sub
