61c505fb7666fb7507b9001b40d07d1c7ff1ec76
mtk57 <mtk.kawa@gmail.com>
2024-06-26 20:12:16 +0900

    a

--- .NET/Survey/SimpleLogger/Logger.vb
+++ .NET/Survey/SimpleLogger/Logger.vb
@@ -1,11 +1,19 @@
-Module SimpleLogger
+﻿Imports System.Threading
+Imports System.IO
+Imports System
+Imports System.Reflection
 
-    Public Sub WriteLog(ByVal contents As String)
-        Dim file_num As Integer
-        file_num = FreeFile()
-        FileOpen(file_num, "SimpleLogger.log", OpenMode.Append)
-        PrintLine(file_num, Format(Today, "yyyy/mm/dd") & " " & Format(Now, "hh:mm:ss") & ":" & contents)
-        FileClose(file_num)
-        file_num = -1
-    End Sub
-End Module
+Namespace Logger
+
+    Public Class Logger
+        Public Shared Sub WriteLog(ByVal contents As String)
+            Dim file_num As Integer
+            file_num = FreeFile()
+            FileOpen(file_num, "Logger.log", OpenMode.Append)
+            PrintLine(file_num, Format(Today, "yyyy/mm/dd") & " " & Format(Now, "hh:mm:ss") & ":" & contents)
+            FileClose(file_num)
+            file_num = -1
+        End Sub
+
+    End Class
+End Namespace
