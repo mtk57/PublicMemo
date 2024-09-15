434fe1d8277fa50cc20673c0cf821047be8b2b7e
mtk57 <mtk.kawa@gmail.com>
2024-09-16 07:09:01 +0900

    v1.0.0

--- .NET/Survey/NonSJISCharDetector/Form1.Designer.cs
+++ .NET/Survey/NonSJISCharDetector/Form1.Designer.cs
@@ -29,12 +29,167 @@
         private void InitializeComponent ()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBoxDirPath = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonRefDir = new System.Windows.Forms.Button();
+            this.textBoxExt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxCreateBackup = new System.Windows.Forms.CheckBox();
+            this.textBoxOutDirPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonRefOutDir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxDirPath
+            // 
+            this.textBoxDirPath.AllowDrop = true;
+            this.textBoxDirPath.Location = new System.Drawing.Point(33, 43);
+            this.textBoxDirPath.Name = "textBoxDirPath";
+            this.textBoxDirPath.Size = new System.Drawing.Size(460, 19);
+            this.textBoxDirPath.TabIndex = 0;
+            this.textBoxDirPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxDirPath_DragDrop);
+            this.textBoxDirPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxDirPath_DragEnter);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Target Dir path";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(35, 198);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Replace SPACE";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(214, 252);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(124, 23);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "RUN";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonRefDir
+            // 
+            this.buttonRefDir.Location = new System.Drawing.Point(500, 38);
+            this.buttonRefDir.Name = "buttonRefDir";
+            this.buttonRefDir.Size = new System.Drawing.Size(52, 23);
+            this.buttonRefDir.TabIndex = 5;
+            this.buttonRefDir.Text = "ref";
+            this.buttonRefDir.UseVisualStyleBackColor = true;
+            this.buttonRefDir.Click += new System.EventHandler(this.buttonRefDir_Click);
+            // 
+            // textBoxExt
+            // 
+            this.textBoxExt.Location = new System.Drawing.Point(33, 94);
+            this.textBoxExt.Name = "textBoxExt";
+            this.textBoxExt.Size = new System.Drawing.Size(145, 19);
+            this.textBoxExt.TabIndex = 6;
+            this.textBoxExt.Text = "txt,frm,bas,cls,ctl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Target Extension";
+            // 
+            // checkBoxCreateBackup
+            // 
+            this.checkBoxCreateBackup.AutoSize = true;
+            this.checkBoxCreateBackup.Location = new System.Drawing.Point(184, 198);
+            this.checkBoxCreateBackup.Name = "checkBoxCreateBackup";
+            this.checkBoxCreateBackup.Size = new System.Drawing.Size(100, 16);
+            this.checkBoxCreateBackup.TabIndex = 8;
+            this.checkBoxCreateBackup.Text = "Create Backup";
+            this.checkBoxCreateBackup.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOutDirPath
+            // 
+            this.textBoxOutDirPath.AllowDrop = true;
+            this.textBoxOutDirPath.Location = new System.Drawing.Point(33, 148);
+            this.textBoxOutDirPath.Name = "textBoxOutDirPath";
+            this.textBoxOutDirPath.Size = new System.Drawing.Size(460, 19);
+            this.textBoxOutDirPath.TabIndex = 9;
+            this.textBoxOutDirPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxOutDirPath_DragDrop);
+            this.textBoxOutDirPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxOutDirPath_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Output Dir path";
+            // 
+            // buttonRefOutDir
+            // 
+            this.buttonRefOutDir.Location = new System.Drawing.Point(500, 148);
+            this.buttonRefOutDir.Name = "buttonRefOutDir";
+            this.buttonRefOutDir.Size = new System.Drawing.Size(52, 23);
+            this.buttonRefOutDir.TabIndex = 11;
+            this.buttonRefOutDir.Text = "ref";
+            this.buttonRefOutDir.UseVisualStyleBackColor = true;
+            this.buttonRefOutDir.Click += new System.EventHandler(this.buttonRefOutDir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size( 800, 450 );
+            this.ClientSize = new System.Drawing.Size(562, 299);
+            this.Controls.Add(this.buttonRefOutDir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxOutDirPath);
+            this.Controls.Add(this.checkBoxCreateBackup);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxExt);
+            this.Controls.Add(this.buttonRefDir);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDirPath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxDirPath;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonRefDir;
+        private System.Windows.Forms.TextBox textBoxExt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxCreateBackup;
+        private System.Windows.Forms.TextBox textBoxOutDirPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonRefOutDir;
     }
 }
 
--- .NET/Survey/NonSJISCharDetector/Form1.cs
+++ .NET/Survey/NonSJISCharDetector/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NonSJISCharDetector
@@ -16,41 +12,96 @@ namespace NonSJISCharDetector
         public Form1 ()
         {
             InitializeComponent();
+
+#if DEBUG
+            textBoxDirPath.Text = @"C:\_git\PublicMemo\.NET\Survey\NonSJISCharDetector\testdata";
+            textBoxOutDirPath.Text = @"C:\_git\PublicMemo\.NET\Survey\NonSJISCharDetector\output";
+#endif
+        }
+
+        private void buttonRefDir_Click ( object sender, EventArgs e )
+        {
+            this.textBoxDirPath.Text = ChooseDirPath();
+        }
+
+        private void buttonRefOutDir_Click ( object sender, EventArgs e )
+        {
+            this.textBoxOutDirPath.Text = ChooseDirPath();
+        }
+
+        private void buttonRun_Click ( object sender, EventArgs e )
+        {
+            try
+            {
+                Run();
+
+                MessageBox.Show( "Success" );
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show( $"ERROR! {ex.Message}" );
+            }
         }
 
         private void Run ()
         {
-            Console.WriteLine( "フォルダのパスを入力してください：" );
-            string folderPath = Console.ReadLine();
+            var folderPath = this.textBoxDirPath.Text;
 
-            Console.WriteLine( "検索する拡張子を入力してください（例：.txt）：" );
-            string extension = Console.ReadLine();
+            var extensions = this.textBoxExt.Text.Split( ',' );
 
-            Console.WriteLine( "結果を出力するTSVファイルのパスを入力してください：" );
-            string outputPath = Console.ReadLine();
+            var outputPath = this.textBoxOutDirPath.Text + @"\" + DateTime.Now.ToString( "yyyyMMdd_HHmmss" ) + ".tsv";
 
             List<string> results = new List<string>();
 
-            foreach ( string filePath in Directory.GetFiles( folderPath, $"*{extension}", SearchOption.AllDirectories ) )
+            foreach ( var ext in extensions )
             {
-                CheckFile( filePath, results );
+                foreach ( var filePath in Directory.GetFiles( folderPath, $"*.{ext}", SearchOption.AllDirectories ) )
+                {
+                    FindInvalidShiftJISBytes( filePath, results );
+                }
             }
 
             File.WriteAllLines( outputPath, results, Encoding.UTF8 );
-            Console.WriteLine( $"処理が完了しました。結果は {outputPath} に保存されました。" );
         }
 
-        private void CheckFile ( string filePath, List<string> results )
+        private void FindInvalidShiftJISBytes ( string filePath, List<string> results )
         {
-            byte [] fileBytes = File.ReadAllBytes( filePath );
-            List<(int offset, byte value)> invalidData = new List<(int, byte)>();
+            List<(long offset, byte value)> invalidData = new List<(long, byte)>();
 
-            for ( int i = 0; i < fileBytes.Length; i++ )
+            using ( FileStream fs = new FileStream( filePath, FileMode.Open, FileAccess.Read ) )
             {
-                byte b = fileBytes [ i ];
-                if ( !IsValidShiftJISByte( b ) && !IsControlCharacter( b ) )
+                byte [] buffer = new byte [ 1024 ];
+                int bytesRead;
+                long offset = 0;
+
+                while ( ( bytesRead = fs.Read( buffer, 0, buffer.Length ) ) > 0 )
                 {
-                    invalidData.Add( (i, b) );
+                    for ( int i = 0; i < bytesRead; i++ )
+                    {
+                        byte b1 = buffer [ i ];
+                        if ( IsShiftJISLeadByte( b1 ) )
+                        {
+                            if ( i + 1 < bytesRead )
+                            {
+                                byte b2 = buffer [ i + 1 ];
+                                if ( !IsValidShiftJISCharacter( b1, b2 ) )
+                                {
+                                    invalidData.Add( (offset + i, b1) );
+                                }
+                                i++; // 2バイト文字なので、次のバイトをスキップ
+                            }
+                            else
+                            {
+                                // ファイルの最後で不完全な2バイト文字がある場合
+                                invalidData.Add( (offset + i, b1) );
+                            }
+                        }
+                        else if ( !IsValidShiftJISSingleByte( b1 ) )
+                        {
+                            invalidData.Add( (offset + i, b1) );
+                        }
+                    }
+                    offset += bytesRead;
                 }
             }
 
@@ -63,16 +114,92 @@ namespace NonSJISCharDetector
             }
         }
 
-        private bool IsValidShiftJISByte ( byte b )
+        private bool IsShiftJISLeadByte ( byte b )
         {
-            return ( b >= 0x20 && b <= 0x7E ) || ( b >= 0xA1 && b <= 0xDF ) || ( b >= 0x81 && b <= 0x9F ) || ( b >= 0xE0 && b <= 0xFC );
+            return ( b >= 0x81 && b <= 0x9F ) || ( b >= 0xE0 && b <= 0xFC );
         }
 
-        private bool IsControlCharacter ( byte b )
+        private bool IsValidShiftJISSingleByte ( byte b )
         {
-            return b < 0x20 || b == 0x7F;
+            return ( b >= 0x20 && b <= 0x7E ) || ( b >= 0xA1 && b <= 0xDF );
+        }
+
+        private bool IsValidShiftJISCharacter ( byte b1, byte b2 )
+        {
+            if ( b1 >= 0x81 && b1 <= 0x9F )
+            {
+                return ( b2 >= 0x40 && b2 <= 0x7E ) || ( b2 >= 0x80 && b2 <= 0xFC );
+            }
+            else if ( b1 >= 0xE0 && b1 <= 0xFC )
+            {
+                return ( b2 >= 0x40 && b2 <= 0x7E ) || ( b2 >= 0x80 && b2 <= 0xFC );
+            }
+            return false;
+        }
+
+        private string ChooseDirPath ()
+        {
+            var fbd = new FolderBrowserDialog();
+
+            fbd.Description = "Choose directory";
+
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            fbd.SelectedPath = @"C:\Windows";
+
+            fbd.ShowNewFolderButton = true;
+
+            if ( fbd.ShowDialog( this ) == DialogResult.OK )
+            {
+                return fbd.SelectedPath;
+            }
+
+            return string.Empty;
+        }
+
+        private void textBoxDirPath_DragDrop ( object sender, DragEventArgs e )
+        {
+            var path = getDropData( e );
+
+            if ( !Directory.Exists( path ) ) return;
+
+            textBoxDirPath.Text = path;
+        }
+
+        private void textBoxDirPath_DragEnter ( object sender, DragEventArgs e )
+        {
+            startDragDrop( e );
+        }
+
+        private void textBoxOutDirPath_DragDrop ( object sender, DragEventArgs e )
+        {
+            var path = getDropData( e );
+
+            if ( !Directory.Exists( path ) ) return;
+
+            textBoxOutDirPath.Text = path;
+        }
+
+        private void textBoxOutDirPath_DragEnter ( object sender, DragEventArgs e )
+        {
+            startDragDrop( e );
+        }
+
+        private void startDragDrop ( DragEventArgs e )
+        {
+            if ( e.Data.GetDataPresent( DataFormats.FileDrop ) )
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private string getDropData ( DragEventArgs e )
+        {
+            var fileName = ( string [] ) e.Data.GetData( DataFormats.FileDrop, false );
+
+            if ( fileName == null || fileName.Length == 0 || fileName.Length > 1 ) return string.Empty;
+
+            return fileName [ 0 ];
         }
     }
-
-
 }
