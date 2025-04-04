5f2049bf63210882ab442f351a6e22f11e03032a
mtk57 <mtk.kawa@gmail.com>
2025-04-04 22:03:42 +0900

    a

---
+++ .NET/SimpleFileSearch/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SimpleFileSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.chkUseRegex = new System.Windows.Forms.CheckBox();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            this.columnFilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(666, 35);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "ref";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(46, 35);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(614, 19);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search dir path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search file name";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(48, 97);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(614, 19);
+            this.txtKeyword.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(337, 146);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // chkUseRegex
+            // 
+            this.chkUseRegex.AutoSize = true;
+            this.chkUseRegex.Location = new System.Drawing.Point(48, 132);
+            this.chkUseRegex.Name = "chkUseRegex";
+            this.chkUseRegex.Size = new System.Drawing.Size(88, 16);
+            this.chkUseRegex.TabIndex = 6;
+            this.chkUseRegex.Text = "RegEx Mode";
+            this.chkUseRegex.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.AllowUserToAddRows = false;
+            this.dataGridViewResults.AllowUserToDeleteRows = false;
+            this.dataGridViewResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnFilePath});
+            this.dataGridViewResults.Location = new System.Drawing.Point(48, 195);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.ReadOnly = true;
+            this.dataGridViewResults.Size = new System.Drawing.Size(678, 231);
+            this.dataGridViewResults.TabIndex = 5;
+            this.dataGridViewResults.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewResults_CellDoubleClick);
+            // 
+            // columnFilePath
+            // 
+            this.columnFilePath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.columnFilePath.HeaderText = "ファイルパス";
+            this.columnFilePath.Name = "columnFilePath";
+            this.columnFilePath.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 463);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.chkUseRegex);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Simple File Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.CheckBox chkUseRegex;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnFilePath;
+    }
+}
+
---
+++ .NET/SimpleFileSearch/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFileSearch
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click ( object sender, EventArgs e )
+        {
+            // フォルダ選択ダイアログを表示
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "検索するフォルダを選択してください";
+                
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolderPath.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btnSearch_Click ( object sender, EventArgs e )
+        {
+                        // バリデーションチェック
+            if (string.IsNullOrWhiteSpace(txtKeyword.Text))
+            {
+                MessageBox.Show("検索キーワードを入力してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtKeyword.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtFolderPath.Text))
+            {
+                MessageBox.Show("検索するフォルダを選択してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFolderPath.Focus();
+                return;
+            }
+
+            if (!Directory.Exists(txtFolderPath.Text))
+            {
+                MessageBox.Show("指定されたフォルダが存在しません。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFolderPath.Focus();
+                return;
+            }
+
+            // 以前の検索結果をクリア
+            dataGridViewResults.Rows.Clear();
+
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+
+                string searchPattern = txtKeyword.Text;
+                bool useRegex = chkUseRegex.Checked;
+                
+                List<string> foundFiles = new List<string>();
+
+                // ファイル検索
+                if (useRegex)
+                {
+                    // 正規表現モード
+                    try
+                    {
+                        Regex regex = new Regex(searchPattern, RegexOptions.IgnoreCase);
+                        
+                        foreach (string file in Directory.GetFiles(txtFolderPath.Text, "*.*", SearchOption.AllDirectories))
+                        {
+                            string fileName = Path.GetFileName(file);
+                            if (regex.IsMatch(fileName))
+                            {
+                                foundFiles.Add(file);
+                            }
+                        }
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        MessageBox.Show($"無効な正規表現です: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else
+                {
+                    // ワイルドカードモード
+                    try
+                    {
+                        // ワイルドカード検索にはWindows組み込みのワイルドカードサポートを使用
+                        foundFiles.AddRange(Directory.GetFiles(txtFolderPath.Text, searchPattern, SearchOption.AllDirectories));
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        MessageBox.Show($"無効な検索パターンです: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+                // 結果を表示
+                foreach (string file in foundFiles)
+                {
+                    dataGridViewResults.Rows.Add(file);
+                }
+
+                // 結果件数を表示
+                this.Text = $"シンプルなファイル検索 - {foundFiles.Count} 件のファイルが見つかりました";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"エラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void dataGridViewResults_CellDoubleClick ( object sender, DataGridViewCellEventArgs e )
+        {
+            if (e.RowIndex >= 0 && dataGridViewResults.Rows[e.RowIndex].Cells[0].Value != null)
+            {
+                string filePath = dataGridViewResults.Rows[e.RowIndex].Cells[0].Value.ToString();
+                
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+                        // エクスプローラーでフォルダを開いてファイルを選択
+                        Process.Start("explorer.exe", $"/select,\"{filePath}\"");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"フォルダを開けませんでした: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
